174d9d6f07d2720cd55b547992423e743706887d
DPadillaNYG <DPadilla.NYG@gmail.com>
2018-11-10 07:28:08 -0500

    New Logic

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-
+<!-- 
 # jQuery Assignment
 
 ### Option Two: Star Wars RPG Game (Challenge)
 
 1. [Watch the demo](https://youtu.be/klN2-ITjRt8).
 
-2. Here's how the app works:
+2. Here's how the app works: -->
 
    * When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game.
 
@@ -33,7 +33,7 @@
   * For example, if the base Attack Power is 6, each attack will increase the Attack Power by 6 (12, 18, 24, 30 and so on).
 * The enemy character only has `Counter Attack Power`. 
 
-  * Unlike the player's `Attack Points`, `Counter Attack Power` never changes.
+  <!-- * Unlike the player's `Attack Points`, `Counter Attack Power` never changes. -->
 
 <!-- * The `Health Points`, `Attack Power` and `Counter Attack Power` of each character must differ. -->
 
@@ -41,7 +41,7 @@
 
   * A winning player must pick their characters wisely by first fighting an enemy with low `Counter Attack Power`. This will allow them to grind `Attack Power` and to take on enemies before they lose all of their `Health Points`. Healing options would mess with this dynamic.
 
-* Your players should be able to win and lose the game no matter what character they choose. The challenge should come from picking the right enemies, not choosing the strongest player.
+<!-- * Your players should be able to win and lose the game no matter what character they choose. The challenge should come from picking the right enemies, not choosing the strongest player.
 
 ### Reminder: Submission on BCS
 
@@ -63,4 +63,4 @@ Add a `README.md` to your repository describing the project. Here are some resou
 ### Add To Your Portfolio
 
 After completing the homework please add the piece to your portfolio. Make sure to add a link to your updated portfolio in the comments section of your homework so the TAs can easily ensure you completed this step when they are grading the assignment. To receive an 'A' on any assignment, you must link to it from your portfolio.
-
+ -->
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,27 +2,28 @@
 var jonSnow = {
     hp : 100,
     atkPower : 25,
-    counterAtkPower : Math.floor(Math.random * 15 + 1)
+    counterAtk : 15
 };
 
 var madQueen = {
     hp: 125,
     atkPower: 15,
-    counterAtkPower: Math.floor(Math.random * 10 + 1)
+    counterAtk: 10
 };
 
 var lastTargaryen = {
     hp: 150,
     atkPower: 20,
-    counterAtkPower: Math.floor(Math.random * 20 + 1)
+    counterAtk: 20
 };
 
 var nightKing = {
     hp: 200,
     atkPower: 10,
-    counterAtkPower: Math.floor(Math.random * 25 + 1)
+    counterAtk: 25
 };
 
+// Element Access Variables
 var starkCard = $('.access-stark');
 var lannisterCard = $('.access-lannister');
 var targaryenCard = $('.access-targaryen');
@@ -38,4 +39,48 @@ var atkResults = $('#attacker-results');
 var defResults = $('#defender-results');
 var attackButton = $('#attack-button');
 var restartButton = $('#restart-button');
+var atkPosition = $('.atk-position');
+var defPosition = $('.def-position');
+var flagHero = false;
+var flagEnemy = false;
+var gotMusic = $("#got-music");
+
+function playMusic () {
+    gotMusic[0].play();
+}
+
+restartButton.click(function() {
+    location.reload();
+});
+
+function moveToAttacker() {
+    $(document).on("click", function (e) {
+        var target = e.target.parentNode;
+        if((!flagHero) && (target.classList.contains('access-stark') || 
+                           target.classList.contains('access-lannister') ||
+                           target.classList.contains('access-targaryen') || 
+                           target.classList.contains('access-walker'))) {
+            atkPosition.append(target);
+            flagHero = true;
+            headerText.text("Declare Your Enemy");
+        }
+    });
+}
+
+function moveToDefender() {
+    $(document).on("click", function (e) {
+        var target = e.target.parentNode;
+        if (defPosition.children().length !== 4 && (target.parentNode.classList.contains('access-stark2') || 
+                                                    target.parentNode.classList.contains('access-lannister2') ||
+                                                    target.parentNode.classList.contains('access-targaryen2') || 
+                                                    target.parentNode.classList.contains('access-walker2'))) {
+            defPosition.append(target);
+        }
+    });
+}
+
+playMusic();
+moveToDefender();
+moveToAttacker();
+
 
