2c29cf20e2b97c428e0a7e50e1fd730a47d13ae9
DPadillaNYG <DPadilla.NYG@gmail.com>
2018-11-11 04:09:36 -0500

    New Designs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,52 +4,52 @@ var westeros = {
     jonSnow : {
         name : "Jon Snow",
         hp : 100,
-        atkPower: 25,
-        grindAtkPower : function () {
-            this.atkPower += 25;
+        atkBoost : function () {
+            atkPower += 25;
+            return atkPower;
         },
         counterAtk : 15,
         updateHpText : function () {
-            $('.stark-hp').text("HP: " + this.hp);
+            $('.stark-hp').text("hp: " + this.hp);
         }
     },
 
     madQueen : {
         name : "Cersei Lannister",
         hp : 125,
-        atkPower : 15,
-        grindAtkPower : function () {
-            this.atkPower += 15;
+        atkBoost : function () {
+            atkPower += 15;
+            return atkPower;
         },
         counterAtk : 10,
         updateHpText : function () {
-            $('.lannister-hp').text("HP: " + this.hp);
+            $('.lannister-hp').text("hp: " + this.hp);
         }
     },
 
     lastTargaryen : {
         name : "Daenerys Targaryen",
         hp : 150,
-        atkPower : 20,
-        grindAtkPower : function () {
-            this.atkPower += 20;
+        atkBoost : function () {
+            atkPower += 20;
+            return atkPower;
         },
         counterAtk : 20,
         updateHpText : function () {
-            $('.targaryen-hp').text("HP: " + this.hp);
+            $('.targaryen-hp').text("hp: " + this.hp);
         }
     },
 
     nightKing : {
         name : "The Night King",
         hp : 200,
-        atkPower : 10,
-        grindAtkPower : function () {
-            this.atkPower += 10;
+        atkBoost : function () {
+            atkPower += 10;
+            return atkPower;
         },
         counterAtk : 25,
         updateHpText : function () {
-            $('.walker-hp').text("HP: " + this.hp);
+            $('.walker-hp').text("hp: " + this.hp);
         }
     }
 
@@ -63,8 +63,11 @@ var $attackButton = $('#attack-button');
 var $restartButton = $('#restart-button');
 var $atkPosition = $('.atk-position');
 var $defPosition = $('.def-position');
+
+var atkPower = 0;
 var attackerCard;
 var defenderCard;
+var defeatedOpp;
 var flagHero = false;
 var flagEnemy = false;
 
@@ -97,6 +100,7 @@ function assignStage() {
 
         } else if (!flagEnemy) {
             $defPosition.append(this);
+            defeatedOpp = this;
             defenderCard = extractData(this.id);
             flagEnemy = true;
         }
@@ -108,14 +112,28 @@ function attackStage() {
         if (!attackerCard || !defenderCard) {
             return;     
         }
+        defenderCard.hp -= attackerCard.atkBoost();
         attackerCard.hp -= defenderCard.counterAtk;
-        defenderCard.hp -= attackerCard.atkPower;
         attackerCard.updateHpText();
         defenderCard.updateHpText();
-        $atkResults.text("You have decreased " + defenderCard.name + "'s soldier numbers by " + attackerCard.atkPower + "pts.");
+        $atkResults.text("You have decreased " + defenderCard.name + "'s soldier numbers by " + atkPower + "pts.");
         $defResults.text(defenderCard.name + " has decreased your soldier numbers by " + defenderCard.counterAtk + "pts.");
     });
 }
 
 assignStage();
 attackStage();
+
+// if (attackerCard.hp <= 0) {
+//     attackerCard.hp = 0;
+//     attackerCard.updateHpText();
+//     $atkResults.text("When you play the game of thrones, you win or you die. There is no middle ground.");
+//     $defResults.html(defenderCard.name + " has wiped out your army. Press <em>RESTART</em> to try again.");    
+// }
+
+// if (defenderCard.hp <= 0) {
+//     defeatedOpp.remove();
+//     flagEnemy = false;
+//     $atkResults.text("And the climb to the Iron Throne continues...");
+//     $defResults.text(defenderCard.name + "'s army has been defeated. " + defenderCard.name + " died in combat.");    
+// }
