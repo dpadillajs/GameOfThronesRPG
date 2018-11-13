1d20528b903e7c4f2fcdda437e9354236604d4da
DPadillaNYG <DPadilla.NYG@gmail.com>
2018-11-13 06:24:55 -0500

    Final Changes

--- README.md
+++ README.md
@@ -1,66 +1,50 @@
-<!-- 
-# jQuery Assignment
+# RPG Card Game
+An RPG *Card* Game using new skills learned in JavaScript and jQuery.
 
-### Option Two: Star Wars RPG Game (Challenge)
+**Browser compatibility checked with:**
+* Google Chrome - Version 70.0.3538.77 and up
+* Firefox Developer Edition - 64.0b8 (64-bit) and up
+* Safari - Version 12.0.1 and up
 
-1. [Watch the demo](https://youtu.be/klN2-ITjRt8).
+### Assignment
+I was tasked with having to make an RPG game using refined skills in JavaScript
+and jQuery. I decided to use my favorite TV show of all time **Game of Thrones** 
+to base my game around it's lore. It fit perfectly as a card game, so I decided
+to use my CSS skills with CSS Grid, Flex, and Positioning to make it come to life.
+Logic-wise, I had to assign these cards to different roles giving them
+unique attributes to their health, attack power, and counter attack power. I also had
+to make sure that the game would continue past the first battle until every enemy
+had been defeated. Only then would the game end and the winner be crowned King/Queen
+of King's Landing.
 
-2. Here's how the app works: -->
+### Skills Learned
+I learned many things while doing this project with the support of my wife 
+who is also a very talented programmer and utilizing Google-Fu. Here are some things
+I learned on the fly:
 
-   <!-- * When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game. -->
+* object-oriented programming
+* switch statements
+* const variables
+* this
+* setTimeout()
+* .parentNode
+* .classList
+* .contains()
 
-   <!-- * The player must then defeat all of the remaining fighters. Enemies should be moved to a different area of the screen. -->
+**Also, feel free to refer to the comments within game.js to see how I integrated:**
+* jQuery
+* DOM traversal 
+* functions
+* variables
+* if statements
+* click events
+* .append()
+* .remove()
 
-   <!-- * The player chooses an opponent by clicking on an enemy's picture. -->
+### Comments
+Overall, I had so much fun with this assignment. My CSS skills have really come
+a long way. I am a perfectionist by nature, so I never stop tweaking stuff lol.
+So many sleepless nights. I think at one point I stayed up for 45 hours straight, 
+but it was so worth it. Coding is awesome!!!
 
-   <!-- * Once the player selects an opponent, that enemy is moved to a `defender area`. -->
-
-   <!-- * The player will now be able to click the `attack` button.
-     * Whenever the player clicks `attack`, their character damages the defender. The opponent will lose `HP` (health points). These points are displayed at the bottom of the defender's picture.  -->
-<!-- The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their `HP`. These points are shown at the bottom of the player character's picture. -->
-
-<!-- 3. The player will keep hitting the attack button in an effort to defeat their opponent. -->
-
-   <!-- * When the defender's `HP` is reduced to zero or below, remove the enemy from the `defender area`. The player character can now choose a new opponent.
-
-4. The player wins the game by defeating all enemy characters. The player loses the game the game if their character's `HP` falls to zero or below. -->
-
-<!-- ##### Option 2 Game design notes -->
-
-<!-- * Each character in the game has 3 attributes: `Health Points`, `Attack Power` and `Counter Attack Power`. -->
-
-<!-- * Each time the player attacks, their character's Attack Power increases by its base Attack Power. 
-  * For example, if the base Attack Power is 6, each attack will increase the Attack Power by 6 (12, 18, 24, 30 and so on).
-* The enemy character only has `Counter Attack Power`.  -->
-
-  <!-- * Unlike the player's `Attack Points`, `Counter Attack Power` never changes. -->
-
-<!-- * The `Health Points`, `Attack Power` and `Counter Attack Power` of each character must differ. -->
-
-<!-- * No characters in the game can heal or recover Health Points.  -->
-
-  <!-- * A winning player must pick their characters wisely by first fighting an enemy with low `Counter Attack Power`. This will allow them to grind `Attack Power` and to take on enemies before they lose all of their `Health Points`. Healing options would mess with this dynamic. -->
-
-<!-- * Your players should be able to win and lose the game no matter what character they choose. The challenge should come from picking the right enemies, not choosing the strongest player.
-
-### Reminder: Submission on BCS
-
-* Please submit both the deployed Github.io link to your homework AND the link to the Github Repository!
-
-- - -
-
-
-### Create a README.md
-
-Add a `README.md` to your repository describing the project. Here are some resources for creating your `README.md`. Here are some resources to help you along the way:
-
-* [About READMEs](https://help.github.com/articles/about-readmes/)
-
-* [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
-
-- - -
-
-### Add To Your Portfolio
-
-After completing the homework please add the piece to your portfolio. Make sure to add a link to your updated portfolio in the comments section of your homework so the TAs can easily ensure you completed this step when they are grading the assignment. To receive an 'A' on any assignment, you must link to it from your portfolio.
- -->
+*created by David M. Padilla*
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,108 +2,139 @@
 var westeros = {
 
     jonSnow : {
-        name : "Jon Snow",
-        hp : 10000,
-        atkBoost : function () {
-            atkPower += 2250;
-            return atkPower;
-        },
-        counterAtk : 1750,
+
+                name : "Jon Snow",
+                  hp : 10000,
+            atkBoost : function () {
+                            atkPower += 2250;
+                            return atkPower;
+                       },
+          counterAtk : 1750,
         updateHpText : function () {
-            $('.stark-hp').text(this.hp);
-        }
+                            $( '.stark-hp' ).text( this.hp );
+                       }
+
     },
 
     madQueen : {
-        name : "Cersei Lannister",
-        hp : 12500,
-        atkBoost : function () {
-            atkPower += 1750;
-            return atkPower;
-        },
-        counterAtk : 2000,
+
+                name : "Cersei Lannister",
+                  hp : 12500,
+            atkBoost : function () {
+                            atkPower += 1750;
+                            return atkPower;
+                       },
+          counterAtk : 2000,
         updateHpText : function () {
-            $('.lannister-hp').text(this.hp);
-        }
+                            $( '.lannister-hp' ).text( this.hp );
+                       }
+
     },
 
     lastTargaryen : {
-        name : "Daenerys Targaryen",
-        hp : 15000,
-        atkBoost : function () {
-            atkPower += 1250;
-            return atkPower;
-        },
-        counterAtk : 2250,
+
+                name : "Daenerys Targaryen",
+                  hp : 15000,
+            atkBoost : function () {
+                            atkPower += 1250;
+                            return atkPower;
+                       },
+          counterAtk : 2250,
         updateHpText : function () {
-            $('.targaryen-hp').text(this.hp);
-        }
+                            $( '.targaryen-hp' ).text( this.hp );
+                       }
+
     },
 
     nightKing : {
-        name : "The Night King",
-        hp : 20000,
-        atkBoost : function () {
-            atkPower += 1000;
-            return atkPower;
-        },
-        counterAtk : 2750,
+
+                name : "The Night King",
+                  hp : 20000,
+            atkBoost : function () {
+                            atkPower += 1000;
+                            return atkPower;
+                       },
+          counterAtk : 2750,
         updateHpText : function () {
-            $('.walker-hp').text(this.hp);
-        }
+                            $( '.walker-hp' ).text( this.hp );
+                       }
+
     }
 
 }
 
 // Element Access Variables
-var $headerText = $('#user-choice h1');
-var $atkResults = $('#attacker-results');
-var $defResults = $('#defender-results');
-var $attackButton = $('#attack-button');
-var $restartButton = $('#restart-button');
-var $atkPosition = $('.atk-position');
-var $defPosition = $('.def-position');
-var assignSound = $('#bell-sound');
-var attackSound = $('#atk-sound');
-var autoplaySong = $('#got-music');
-var autoplaySnow = $('#snow-music');
+var $headerText = $( '#user-choice h1' );
+var $atkResults = $( '#attacker-results' );
+var $defResults = $( '#defender-results' );
+var $attackButton = $( '#attack-button' );
+var $restartButton = $( '#restart-button' );
+var $atkPosition = $( '.atk-position' );
+var $defPosition = $( '.def-position' );
+var assignSound = $( '#bell-sound' );
+var attackSound = $( '#atk-sound' );
+var autoplaySong = $( '#got-music' );
+var autoplaySnow = $( '#snow-music' );
 
-var battleCount = 0;
+// Game-specific variables
+var deadEnemies = 0;
 var atkPower = 0;
-var totalDeadSoldiersFromAttacker;
+var totalAttackerDeadForces;
 
+// Variables used for OOP & DOM manipulation
 var attackerCard;
 var defenderCard;
 var $enemyDiv;
 var $heroDiv;
 
+// Flag variables
 var blockFromChoosingAttacker = false;
 var blockFromChoosingDefender = false;
 var ignoreAtkBtnClick = false;
 
+// This function will play an atmospheric snow blizzard 
+// as well as a Game of Thrones song indefinitely once called
 function autoPlay() {
+
     autoplaySong[0].play();
     autoplaySnow[0].play();
-    autoplaySnow.prop("volume", 0.4);
+    autoplaySnow.prop( "volume", 0.4 );
+
 }
 
+// This function will play a sword slashing sound effect 
+// when the attack button is pressed.
 function playOnAtkBtnClick() {
+
     attackSound[0].play();
-    attackSound.prop("volume", 0.25);
+    attackSound.prop( "volume", 0.25 );
+
 }
 
+// This function will play an alert sound effect when a card is chosen
 function playIfCardIsChosen() {
+
     assignSound[0].play();
-    assignSound.prop("volume", 0.5);
+    assignSound.prop( "volume", 0.5 );
+
 }
 
+// This function will refresh the window page and reset all progress 
+// on the restart button
 $restartButton.click(function() {
+
     location.reload();
+
 });
 
-function extractData(cardId) {
+// This function is used to link the clicked element
+// to the character object by searching for a specific Id tag
+function extractData( cardId ) {
+
     var card;
-    switch (cardId) {
+
+    switch ( cardId ) {
+
         case "starks-card" : card = westeros.jonSnow;
             break;
         case "walkers-card" : card = westeros.nightKing;
@@ -112,81 +143,137 @@ function extractData(cardId) {
             break;
         case "targaryens-card" : card = westeros.lastTargaryen;
             break;
+
     }
+
     return card;
+
 }
 
+// This function allows the user to choose an attacker and a defender
 function assignStage() {
-    $("[js-battleCard]").on("click", function() {
-        if(!blockFromChoosingAttacker) {
+    
+    $( "[js-battleCard]" ).on( "click", function() {
+
+        if( !blockFromChoosingAttacker ) {
+
             autoPlay();
             playIfCardIsChosen();      
-            $atkPosition.append(this);
+            $atkPosition.append( this );
             $heroDiv = this;
-            attackerCard = extractData(this.id);
+            attackerCard = extractData( this.id );
             blockFromChoosingAttacker = true;
-            $headerText.text("Declare Your Enemy");
-            const initialAttackerHP = attackerCard.hp;
-            totalDeadSoldiersFromAttacker = initialAttackerHP;
+            $headerText.text( "Declare Your Enemy" );
 
-        } else if (!blockFromChoosingDefender && !(this.parentNode.classList.contains("atk-position"))) {
-            $("a").attr("href", "#iron-throne");
+            // We prevent the reassigning of attackerCard.hp within the const
+            // variable and then we assign it to the global variable to be
+            // accessible in the attackStage function below
+            const initialAttackerHP = attackerCard.hp; 
+            totalAttackerDeadForces = initialAttackerHP;
+
+        } else if (!blockFromChoosingDefender && 
+                   !( this.parentNode.classList.contains( "atk-position" ) )) {
+
             playIfCardIsChosen();
-            $defPosition.append(this);
+            $( "a" ).attr( "href", "#def-position" );
+            $defPosition.append( this );
             ignoreAtkBtnClick = false;    
             $enemyDiv = this;
-            defenderCard = extractData(this.id);
-            $atkResults.text('"The army awaits your command."');
-            $defResults.text(defenderCard.name + "'s army draws near.");
+            defenderCard = extractData( this.id );
+
+            $atkResults.text( "The army awaits your command." );
+            $defResults.text( defenderCard.name + "'s army draws near." );
+
             blockFromChoosingDefender = true;
+
         }
+
     });
+
 }
 
+// This function holds all the possible outcomes and results of an attack
 function attackStage() {
-    $("#attack-button").on("click", function() {
-        if (!attackerCard || !defenderCard) {
+
+    $attackButton.on( "click", function() {
+
+        if ( !attackerCard || !defenderCard ) {
             return;     
         }
-        if (ignoreAtkBtnClick) {
+
+        if ( ignoreAtkBtnClick ) {
             return;
         }
-        defenderCard.hp -= attackerCard.atkBoost();
+
         playOnAtkBtnClick();
-        if (defenderCard.hp <= 0) {
+        defenderCard.hp -= attackerCard.atkBoost();
+
+        if ( defenderCard.hp <= 0 ) {
+
             $enemyDiv.remove();
-            battleCount++;
+            deadEnemies++;
             blockFromChoosingDefender = false;
-            $atkResults.text("And the climb to the Iron Throne continues...");
-            $defResults.text(defenderCard.name + "'s army has been defeated. " + defenderCard.name + " died in combat.");
+
+            $atkResults.text( "And the climb to the Iron Throne continues..." );
+            $defResults.text( defenderCard.name + "'s army has been defeated. " + 
+                              defenderCard.name + " died in combat." );
+
             ignoreAtkBtnClick = true; 
-            if (battleCount === 3) {
+
+            if ( deadEnemies === 3 ) {
+
                 blockFromChoosingDefender = true;
                 ignoreAtkBtnClick = true; 
-                $('#throne-seat').append($heroDiv);
-                $atkResults.text((totalDeadSoldiersFromAttacker -= attackerCard.hp) + " of your men have died to break the wheel. The Iron Throne is yours.");
-                $defResults.text("Power resides where men believe it resides. It's a trick. A shadow on the wall...");
+                $( '#throne-seat' ).append( $heroDiv );
+
+                $headerText.text( "VICTORY" );
+                $atkResults.text( ( totalAttackerDeadForces -= attackerCard.hp ) + 
+                                    " of your men have died to break the wheel. " + 
+                                    "The Iron Throne is yours." );
+                $defResults.text( "Power resides where men believe it resides. " + 
+                                  "It's a trick. A shadow on the wall..." );
+
                 return;
+
             }   
+
         }
+
         defenderCard.updateHpText();
-        if (attackerCard.hp > 0 && defenderCard.hp > 0) {
+
+        if ( attackerCard.hp > 0 && defenderCard.hp > 0 ) {
+
             attackerCard.hp -= defenderCard.counterAtk;
-            if (attackerCard.hp <= 0) {
+
+            if ( attackerCard.hp <= 0 ) {
+
                 blockFromChoosingDefender = true;
                 ignoreAtkBtnClick = true;    
                 attackerCard.hp = 0;
                 attackerCard.updateHpText();
-                $atkResults.text("When you play the game of thrones, you win or you die. There is no middle ground.");
-                $defResults.text(defenderCard.name + " has wiped out your army. Press RESTART to try again.");    
+
+                $atkResults.text( "When you play the game of thrones, you win or you die. " + 
+                                  "There is no middle ground." );
+                $defResults.text( defenderCard.name + " has wiped out your army. " + 
+                                  "Press RESTART to try again." );  
+
             } else {
+
                 attackerCard.updateHpText();
-                $atkResults.text("You have decreased " + defenderCard.name + "'s forces by " + atkPower + ".");
-                $defResults.text(defenderCard.name + " has decreased your forces by " + defenderCard.counterAtk + ".");    
+
+                $atkResults.text( "You have decreased " + defenderCard.name + 
+                                  "'s forces by " + atkPower + "." );
+                $defResults.text( defenderCard.name + " has decreased your forces by " + 
+                                  defenderCard.counterAtk + "." );  
+
             }
+
         }
+
     });
+
 }
 
+// Function Calls
 assignStage();
 attackStage();
